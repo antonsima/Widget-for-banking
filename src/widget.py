d450b6bbd02f1bf99e742ef530f65fe906893f9f
Anton <anton_sima@mail.ru>
2024-12-16 20:33:06 +0500

    Добавлена новая функция для получения даты из входных данных

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(type_and_account_or_card_number: str) -> str:
     """Возвращает строку с замаскированным номером"""
@@ -8,7 +9,7 @@ def mask_account_card(type_and_account_or_card_number: str) -> str:
     type_account_or_card = tmp_list[0]
     account_or_card_number = int(tmp_list[-1])
 
-    if type_account_or_card == 'Счет':
+    if type_account_or_card == "Счет":
         tmp_list.pop()
 
         tmp_list.append(get_mask_account(account_or_card_number))
@@ -17,6 +18,17 @@ def mask_account_card(type_and_account_or_card_number: str) -> str:
 
         tmp_list.append(get_mask_card_number(account_or_card_number))
 
-    masked_account_card = ' '.join(tmp_list)
+    masked_account_card = " ".join(tmp_list)
 
     return masked_account_card
+
+
+def get_date(date_and_time: str) -> str:
+    """Возвращает только дату из входных данных"""
+
+    yyyy_mm_dd = date_and_time.split("T")[0]  # Дата формата ГГГГ-ММ-ДД
+    dd_mm_yyyy = yyyy_mm_dd.split("-")[::-1]  # Дата формата [ДД, ММ, ГГГГ]
+
+    date = ".".join(dd_mm_yyyy)  # Дата формата ДД.ММ.ГГГГ
+
+    return date
