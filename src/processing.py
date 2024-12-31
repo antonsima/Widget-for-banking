344eb80a122e983d9023bd39f838d10aab32bafe
Anton <anton_sima@mail.ru>
2024-12-31 12:35:36 +0500

    Написаны тесты для модуля processing, дополнены тесты для модуля widget и добавлены фикстуры

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from src.widget import get_date
+
+
 def filter_by_state(operation_info: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """
     Возвращает список операций по ключу state
@@ -16,6 +19,11 @@ def sort_by_date(operation_info: list[dict], is_reversed: bool = True) -> list[d
     """
     Возвращает список операций, отсортированный по дате
     """
+
+    for operation in operation_info:
+        if (get_date(operation['date']) == 'Введите корректный формат даты' or
+                get_date(operation['date']) == 'В дате должны содержаться только цифры'):
+            return 'Введите корректный формат даты'
 
     sorted_by_date = operation_info.copy()
     sorted_by_date.sort(key=lambda x: x['date'], reverse=is_reversed)
---
+++ tests/test_processing.py
@@ -0,0 +1,18 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+def test_filter_by_state(test_operations, executed_operations, canceled_operations):
+    assert filter_by_state(test_operations, 'EXECUTED') == executed_operations
+    assert filter_by_state(test_operations, 'CANCELED') == canceled_operations
+    assert filter_by_state(test_operations, 'Что-то другое') == []
+    assert filter_by_state([], 'CANCELED') == []
+    assert filter_by_state(test_operations, '') == []
+
+
+def test_sort_by_date(test_operations, direct_sort, reversed_sort, problem_dates_of_operations):
+    assert sort_by_date(test_operations, True) == reversed_sort
+    assert sort_by_date(test_operations, False) == direct_sort
+    assert sort_by_date(problem_dates_of_operations) == 'Введите корректный формат даты'
+    assert sort_by_date([]) == []
