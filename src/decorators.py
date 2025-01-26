d5d8903424951304c0a66b715f777b5de264c501
Anton <anton_sima@mail.ru>
2025-01-26 13:28:04 +0500

    Стартовая точка для написания аннотации типов

--- src/decorators.py
+++ src/decorators.py
@@ -4,26 +4,25 @@ from functools import wraps
 
 def log(filename=''):
     if filename:
-        filename = os.path.join(os.path.dirname(__file__), "..", "logs", str(filename))
+        PATH_TO_LOGS = os.path.join(os.path.dirname(__file__), "..", "logs", str(filename))
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
             except Exception as e:
-                content = \
-f'''{func.__name__}
+                content = f'''{func.__name__}
 error: тип ошибки {e}
 Входные параметры: {args}, {kwargs}'''
                 if filename:
-                    with open(filename, 'w', encoding='utf-8') as file:
+                    with open(PATH_TO_LOGS, 'w', encoding='utf-8') as file:
                         file.write(f'{content}')
                 return content
-            content = \
-f'''{func.__name__}
+            content = f'''{func.__name__}
 Результат выполнения функции: {result}'''
             if filename:
-                with open(filename, 'w', encoding='utf-8') as file:
+                with open(PATH_TO_LOGS, 'w', encoding='utf-8') as file:
                     file.write(f'{content}')
             return content
         return wrapper
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -9,7 +9,7 @@ def test_log_console():
         return x + y
 
     result = my_function(1, 6)
-    assert result == f'''my_function
+    assert result == '''my_function
 Результат выполнения функции: 7'''
 
 
@@ -19,10 +19,10 @@ def test_log_file():
         return x + y
 
     my_function(1, 6)
-    filename = os.path.join(os.path.dirname(__file__), "..", "logs", 'mylog.txt')
-    with open(filename, 'r', encoding='utf-8') as file:
+    PATH_TO_LOGS = os.path.join(os.path.dirname(__file__), "..", "logs", 'mylog.txt')
+    with open(PATH_TO_LOGS, 'r', encoding='utf-8') as file:
         my_log = file.read()
-    assert my_log == f'''my_function
+    assert my_log == '''my_function
 Результат выполнения функции: 7'''
 
 
@@ -32,9 +32,9 @@ def test_log_console_error():
         return x + y
 
     result = my_function(1, '6')
-    assert result == f'''my_function
+    assert result == '''my_function
 error: тип ошибки unsupported operand type(s) for +: 'int' and 'str'
-Входные параметры: (1, '6'), {{}}'''
+Входные параметры: (1, '6'), {}'''
 
 
 def test_log_file_error():
@@ -43,9 +43,9 @@ def test_log_file_error():
         return x + y
 
     my_function(1, '6')
-    filename = os.path.join(os.path.dirname(__file__), "..", "logs", 'mylog.txt')
-    with open(filename, 'r', encoding='utf-8') as file:
+    PATH_TO_LOGS = os.path.join(os.path.dirname(__file__), "..", "logs", 'mylog.txt')
+    with open(PATH_TO_LOGS, 'r', encoding='utf-8') as file:
         my_log = file.read()
-    assert my_log == f'''my_function
+    assert my_log == '''my_function
 error: тип ошибки unsupported operand type(s) for +: 'int' and 'str'
-Входные параметры: (1, '6'), {{}}'''
+Входные параметры: (1, '6'), {}'''
