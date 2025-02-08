4759acd3e1844c47258ec7ac724e18a7d9824f98
Anton <anton_sima@mail.ru>
2025-02-08 13:07:46 +0500

    Написаны тесты для новых функций, coverage 100%

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,4 +1,4 @@
-from src.processing import filter_by_state, sort_by_date
+from src.processing import filter_by_state, get_category_count, get_transactions_by_pattern, sort_by_date
 
 
 def test_filter_by_state(test_operations, executed_operations, canceled_operations):
@@ -14,3 +14,17 @@ def test_sort_by_date(test_operations, direct_sort, reversed_sort, problem_dates
     assert sort_by_date(test_operations, False) == direct_sort
     assert sort_by_date(problem_dates_of_operations) == []
     assert sort_by_date([]) == []
+
+
+def test_get_transactions_by_pattern(test_transactions):
+    assert get_transactions_by_pattern(test_transactions, 'перевод') == test_transactions
+    assert get_transactions_by_pattern(test_transactions, 'открытие') == []
+
+
+def test_get_category_count(test_transactions):
+    assert get_category_count(test_transactions,
+                              ['Перевод организации',
+                               'Перевод со счета на счет']) == {'Перевод организации': 2,
+                                                                'Перевод со счета на счет': 2}
+    assert get_category_count(test_transactions,
+                              ['Открытие вклада']) == {}
